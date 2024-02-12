3ac843fdb907d895978e66baa955c989ad9ac5e4
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-12 16:26:19 +0100

    Initial commit

---
+++ TheImageComparer.UI/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace TheImageComparer.UI;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public IHost? AppHost { get; private set; }
+
+    public App()
+    {
+        var builder = Host.CreateApplicationBuilder();
+
+        // Add services here
+
+        AppHost = builder.Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit(e);
+    }
+}
+
