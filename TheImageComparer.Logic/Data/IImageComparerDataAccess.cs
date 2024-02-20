8cf34d53334151f55212e4fa700cbba2fa6e08bf
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-20 18:23:34 +0100

    Add GetAllImages() method to DataAccess

--- TheImageComparer.Logic/Data/IImageComparerDataAccess.cs
+++ TheImageComparer.Logic/Data/IImageComparerDataAccess.cs
@@ -1,5 +1,8 @@
-﻿namespace TheImageComparer.Logic.Data;
+﻿using TheImageComparer.Logic.Models;
+
+namespace TheImageComparer.Logic.Data;
 
 public interface IImageComparerDataAccess
 {
+    List<ImageModel> GetAllImages();
 }
--- TheImageComparer.Logic/Data/ImageComparerDataAccess.cs
+++ TheImageComparer.Logic/Data/ImageComparerDataAccess.cs
@@ -1,4 +1,23 @@
-﻿namespace TheImageComparer.Logic.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TheImageComparer.Logic.Models;
+using TheImageComparer.Logic.Services;
+
+namespace TheImageComparer.Logic.Data;
 public class ImageComparerDataAccess : IImageComparerDataAccess
 {
+    private readonly ImageComparerDbContextFactory _contextFactory;
+    private readonly IGetSqliteDbFilePath _dbFilePath;
+
+    public ImageComparerDataAccess(ImageComparerDbContextFactory contextFactory, ISqliteDbFilePathService dbFilePathService)
+    {
+        _contextFactory = contextFactory;
+        _dbFilePath = dbFilePathService;
+    }
+    public List<ImageModel> GetAllImages()
+    {
+        if (_dbFilePath.DbFilePath is null)
+            return [];
+        using var db = _contextFactory.CreateDbContext(_dbFilePath.DbFilePath);
+        return db.Images.AsNoTracking().ToList();
+    }
 }
