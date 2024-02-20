b845a98c5a2768fc15880aae8d9f2f5d37dc5b68
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-20 21:58:39 +0100

    Add OpenDatabaseCommand to  MainMenuViewModel

--- TheImageComparer.UI/ViewModels/MainMenuViewModel.cs
+++ TheImageComparer.UI/ViewModels/MainMenuViewModel.cs
@@ -1,6 +1,31 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TheImageComparer.Logic.Services;
+using TheImageComparer.UI.Services;
 
 namespace TheImageComparer.UI.ViewModels;
 public partial class MainMenuViewModel : ObservableObject
 {
+    private readonly IIOService _ioService;
+    private readonly ISetSqliteDbFilePath _dbFilePath;
+
+    public MainMenuViewModel(IIOService ioService, ISqliteDbFilePathService dbFilePath, IViewManagerService viewManager)
+    {
+        _ioService = ioService;
+        _dbFilePath = dbFilePath;
+        ViewManager = viewManager;
+    }
+
+    public IViewManagerService ViewManager { get; }
+
+    [RelayCommand]
+    private void OpenDatabase()
+    {
+        string? dbFilePath = _ioService.GetFilePathWithDialog("Plik .db|*.db");
+        if (string.IsNullOrWhiteSpace(dbFilePath) == false)
+        {
+            _dbFilePath.DbFilePath = dbFilePath;
+            // navigate to another view
+        }
+    }
 }
