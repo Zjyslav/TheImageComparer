80bda307eb5496fec33eb25e9af44030049fca72
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-20 16:29:16 +0100

    Add Models

---
+++ TheImageComparer.Logic/Models/ImageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TheImageComparer.Logic.Models;
+public class ImageModel
+{
+    public int Id { get; set; }
+    public required string FilePath { get; set; }
+    public bool IsArchived { get; set; } = false;
+    public bool PossibleDuplicate { get; set; } = false;
+}
---
+++ TheImageComparer.Logic/Models/VoteModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TheImageComparer.Logic.Models;
+public class VoteModel
+{
+    public int Id { get; set; }
+    public required ImageModel VotedFor { get; set; }
+    public required ImageModel VotedAgainst { get; set; }
+    public DateTime DateTime { get; set; } = DateTime.UtcNow;
+}
