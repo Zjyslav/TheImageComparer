d128503a7ff27fa9ceb298c36e9c706f54308540
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-21 16:54:17 +0100

    Add ImageFilePathModel

---
+++ TheImageComparer.UI/Models/ImageFilePathModel.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Windows.Media;
+
+namespace TheImageComparer.UI.Models;
+public class ImageFilePathModel
+{
+    public ImageFilePathModel(string filePath)
+    {
+        FilePath = filePath;
+    }
+
+    public string FilePath { get; set; }
+    public string FileName
+    {
+        get
+        {
+            return Path.GetFileName(FilePath);
+        }
+    }
+}
