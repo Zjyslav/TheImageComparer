445a7a611bceb07cbde26bba33ab61435c32107e
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-22 20:50:26 +0100

    Add FileName property to ImageModel

--- TheImageComparer.Logic/Models/ImageModel.cs
+++ TheImageComparer.Logic/Models/ImageModel.cs
@@ -1,8 +1,18 @@
-﻿namespace TheImageComparer.Logic.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheImageComparer.Logic.Models;
 public class ImageModel
 {
     public int Id { get; set; }
     public required string FilePath { get; set; }
     public bool IsArchived { get; set; } = false;
     public bool PossibleDuplicate { get; set; } = false;
+    [NotMapped]
+    public string FileName
+    {
+        get
+        {
+            return Path.GetFileName(FilePath);
+        }
+    }
 }
