be019430d21aabe273d587357176b789c8143067
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-22 20:50:05 +0100

    Add RoundedImage control

---
+++ TheImageComparer.UI/Controls/RoundedImage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TheImageComparer.UI.Controls;
+public partial class RoundedImage : UserControl
+{
+    public static readonly DependencyProperty ImageSourceProperty = DependencyProperty
+        .Register(nameof(ImageSource),
+                  typeof(string),
+                  typeof(RoundedImage));
+    public string ImageSource
+    {
+        get { return (string)GetValue(ImageSourceProperty); }
+        set { SetValue(ImageSourceProperty, value); }
+    }
+    public RoundedImage()
+    {
+        InitializeComponent();
+    }
+}
