bd926cf61a4316739c1e67230b0f8acabf86327f
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-24 00:05:04 +0100

    Add ImageUIModel

---
+++ TheImageComparer.UI/Models/ImageUIModel.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using TheImageComparer.Logic.Models;
+
+namespace TheImageComparer.UI.Models;
+public class ImageUIModel : ImageModel
+{
+    public string FileName
+    {
+        get
+        {
+            return Path.GetFileName(FilePath);
+        }
+    }
+
+    public List<VoteModel> Votes { get; set; } = [];
+    public List<VoteModel> VotesFor
+    {
+        get
+        {
+            return Votes
+                .Where(v => v.VotedFor.Id == Id)
+                .ToList();
+        }
+    }
+    public List<VoteModel> VotesAgainst
+    {
+        get
+        {
+            return Votes
+                .Where(v => v.VotedAgainst.Id == Id)
+                .ToList();
+        }
+    }
+    public int Score { get; set; }
+}
