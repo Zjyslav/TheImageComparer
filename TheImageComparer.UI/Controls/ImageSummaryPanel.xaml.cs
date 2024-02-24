a09bc59dead2a517e9a45c92c6c906b32444dc4c
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-24 01:48:29 +0100

    Add ImageSummaryPanel control

---
+++ TheImageComparer.UI/Controls/ImageSummaryPanel.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TheImageComparer.UI.Models;
+
+namespace TheImageComparer.UI.Controls;
+public partial class ImageSummaryPanel : UserControl
+{
+    public static readonly DependencyProperty ImageSourceProperty = DependencyProperty
+        .Register(nameof(Image),
+                  typeof(ImageUIModel),
+                  typeof(ImageSummaryPanel));
+    public ImageUIModel Image
+    {
+        get { return (ImageUIModel)GetValue(ImageSourceProperty); }
+        set { SetValue(ImageSourceProperty, value); }
+    }
+    public ImageSummaryPanel()
+    {
+        InitializeComponent();
+    }
+}
