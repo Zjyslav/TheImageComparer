d901966875e3a65e0d64e86651c986f578052fd9
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-28 22:50:12 +0100

    Add EnumHelpers
    Not used yet. I still have to figure out a way to use it to show enum display names in UI.

---
+++ TheImageComparer.UI/Helpers/EnumHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace TheImageComparer.UI.Helpers;
+public static class EnumHelpers
+{
+    public static string? GetDisplayName(this Enum enumValue)
+    {
+        return enumValue
+            .GetType()
+            .GetMember(enumValue.ToString())
+            .First()
+            .GetCustomAttribute<DisplayAttribute>()?
+            .GetName();
+    }
+}
