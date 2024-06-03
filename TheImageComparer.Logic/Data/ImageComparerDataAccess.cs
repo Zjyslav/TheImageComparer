8a584e330b1622d9b75a1796488c076c4b9f3f0a
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-06-03 20:01:03 +0200

    Replace InitAndGetDbContext() in DA with a property

--- TheImageComparer.Logic/Data/ImageComparerDataAccess.cs
+++ TheImageComparer.Logic/Data/ImageComparerDataAccess.cs
@@ -8,16 +8,28 @@ public class ImageComparerDataAccess : IImageComparerDataAccess
     private readonly ImageComparerDbContextFactory _contextFactory;
     private readonly IGetSqliteDbFilePath _dbFilePath;
     private ImageComparerDbContext? _dbContext;
+    private ImageComparerDbContext DbContext
+    {
+        get
+        {
+            if (_dbContext is not null)
+                return _dbContext;
+
+            if (_dbFilePath.DbFilePath is not null)
+                _dbContext = _contextFactory.CreateDbContext(_dbFilePath.DbFilePath);
+
+            return _dbContext!;
+        }
+    }
 
     public ImageComparerDataAccess(ImageComparerDbContextFactory contextFactory, IGetSqliteDbFilePath dbFilePath)
     {
         _contextFactory = contextFactory;
         _dbFilePath = dbFilePath;
-
     }
     public IQueryable<ImageModel> GetAllImages()
     {
-        return InitAndGetDbContext().Images
+        return DbContext.Images
             .Include(i => i.VotesFor)
             .Include(i => i.VotesAgainst)
             .AsQueryable();
@@ -25,7 +37,7 @@ public class ImageComparerDataAccess : IImageComparerDataAccess
 
     public IQueryable<VoteModel> GetAllVotes()
     {
-        return InitAndGetDbContext()
+        return DbContext
             .Votes
             .Include(v => v.VotedFor)
             .Include(i => i.VotedAgainst)
@@ -46,14 +58,14 @@ public class ImageComparerDataAccess : IImageComparerDataAccess
     public async Task<IEnumerable<ImageModel>> AddImages(IEnumerable<string> filePaths)
     {
         var images = filePaths.Select(f => new ImageModel() { FilePath = f });
-        InitAndGetDbContext().Images.AddRange(images);
-        await InitAndGetDbContext().SaveChangesAsync();
+        DbContext.Images.AddRange(images);
+        await DbContext.SaveChangesAsync();
         return images;
     }
 
     public async Task<IEnumerable<VoteModel>> GetVotesByImageId(int id)
     {
-        return await InitAndGetDbContext().Votes
+        return await DbContext.Votes
             .Where(v => v.VotedFor.Id == id || v.VotedAgainst.Id == id)
             .ToListAsync();
     }
@@ -63,18 +75,7 @@ public class ImageComparerDataAccess : IImageComparerDataAccess
         var votedAgainst = await GetImageById(votedAgainstImageId);
         if (votedFor is null || votedAgainst is null)
             return;
-        InitAndGetDbContext().Votes.Add(new() { VotedFor = votedFor, VotedAgainst = votedAgainst });
-        await InitAndGetDbContext().SaveChangesAsync();
-    }
-
-    private ImageComparerDbContext InitAndGetDbContext()
-    {
-        if (_dbContext is not null)
-            return _dbContext;
-
-        if (_dbFilePath.DbFilePath is not null)
-            _dbContext = _contextFactory.CreateDbContext(_dbFilePath.DbFilePath);
-
-        return _dbContext!;
+        DbContext.Votes.Add(new() { VotedFor = votedFor, VotedAgainst = votedAgainst });
+        await DbContext.SaveChangesAsync();
     }
 }
