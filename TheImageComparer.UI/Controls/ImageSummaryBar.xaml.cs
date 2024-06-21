58725f8587c438ba7cad81a49fa1c90d56088ae0
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-06-21 15:56:50 +0200

    Add ImageSummaryBar, replace ImageSummaryPanel in VoteView

---
+++ TheImageComparer.UI/Controls/ImageSummaryBar.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TheImageComparer.UI.Models;
+
+namespace TheImageComparer.UI.Controls
+{
+    public partial class ImageSummaryBar : UserControl
+    {
+        public static readonly DependencyProperty ImageProperty = DependencyProperty
+        .Register(nameof(Image),
+                  typeof(ImageUIModel),
+                  typeof(ImageSummaryBar));
+        public ImageUIModel Image
+        {
+            get { return (ImageUIModel)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+        public ImageSummaryBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- TheImageComparer.UI/Controls/ImageSummaryPanel.xaml.cs
+++ TheImageComparer.UI/Controls/ImageSummaryPanel.xaml.cs
@@ -5,14 +5,14 @@ using TheImageComparer.UI.Models;
 namespace TheImageComparer.UI.Controls;
 public partial class ImageSummaryPanel : UserControl
 {
-    public static readonly DependencyProperty ImageSourceProperty = DependencyProperty
+    public static readonly DependencyProperty ImageProperty = DependencyProperty
         .Register(nameof(Image),
                   typeof(ImageUIModel),
                   typeof(ImageSummaryPanel));
     public ImageUIModel Image
     {
-        get { return (ImageUIModel)GetValue(ImageSourceProperty); }
-        set { SetValue(ImageSourceProperty, value); }
+        get { return (ImageUIModel)GetValue(ImageProperty); }
+        set { SetValue(ImageProperty, value); }
     }
     public ImageSummaryPanel()
     {
